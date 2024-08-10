3e0e738c989a8e07d331ce927c5181a3e0c58f18
tekkon <tekkon@tekkons-MacBook-Pro.local>
2024-08-10 12:00:04 +0545

    added new routes in middleware

--- middleware.js
+++ middleware.js
@@ -2,4 +2,9 @@ export { default } from "next-auth/middleware";
 
 export const config = { matcher: [
     "/dashboard",
+    "/listOrder/:path*",
+    "/addOrder/:path*",
+    "/addTable",
+    "/listReport",
+    "/listSales"
 ] };

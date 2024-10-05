4e0b6efb35db29c992a425fa75ec0cd0f2dd15ed
tekkon <tekkon@tekkons-MacBook-Pro.local>
2024-10-06 00:09:38 +0545

    updated discount for KOT only, updated bill flow

--- app/components/AddBillForm.jsx
+++ app/components/AddBillForm.jsx
@@ -130,7 +130,7 @@ export default function AddBillForm({ initialOriginalPrice, initialBillId, initi
         e.preventDefault();
 
         try {
-            const res = await fetch("http://localhost:3000/api/bill", {
+            const res = await fetch("https://billing-nextjs.vercel.app/api/bill", {
                 method: "POST",
                 headers: {
                     "Content-type": "application/json",

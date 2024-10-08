4ed4f8ca3b1debc8acb6949324855d13fc04393c
tekkon <tekkon@tekkons-MacBook-Pro.local>
2024-10-08 16:03:58 +0545

    hookah

--- app/addOrder/[id]/page.jsx
+++ app/addOrder/[id]/page.jsx
@@ -195,6 +195,7 @@ export default function AddOrder({ params }) {
         { value: 'Latte double', label: 'Latte double - NRs 180', price: 180 },
         { value: 'Honey latte', label: 'Honey latte - NRs 200', price: 200 },
         { value: 'Flavoured latte', label: 'Flavoured latte - NRs 200', price: 200 },
+        { value: 'Cappuccino', label: 'Cappuccino - NRs 170', price: 170 },
         { value: 'Flavoured cappuccino', label: 'Flavoured cappuccino - NRs 200', price: 200 },
         { value: 'Hot chocolate', label: 'Hot chocolate - NRs 180', price: 180 },
         { value: 'Flavoured streamed milk', label: 'Flavoured streamed milk - NRs 120', price: 120 },

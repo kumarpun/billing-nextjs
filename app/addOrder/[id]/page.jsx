110104ee8f8f90b65e1f2b9b0e990edd83550604
tekkon <tekkon@tekkons-MacBook-Pro.local>
2024-10-11 18:04:48 +0545

    chef special

--- app/addOrder/[id]/page.jsx
+++ app/addOrder/[id]/page.jsx
@@ -72,6 +72,7 @@ export default function AddOrder({ params }) {
       { value: 'Pizza non-veg', label: 'Pizza non-veg - NRs 400', price: 400 },
       { value: 'Paneer fried rice', label: 'Paneer fried rice - NRs 170', price: 170 },
       { value: 'Paneer noodles', label: 'Paneer noodles - NRs 210', price: 210 },
+      { value: 'Chef special', label: 'Chef special - NRs 600', price: 600 },
 
       { value: 'Hybe special platter veg', label: 'Hybe special platter veg - NRs 700', price: 700 },
       { value: 'Hybe special platter non-veg', label: 'Hybe special platter non-veg - NRs 1050', price: 1050 },

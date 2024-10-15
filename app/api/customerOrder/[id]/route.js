2f31ead508c27eb65766979607d847be13559246
tekkon <tekkon@tekkons-MacBook-Pro.local>
2024-10-15 16:26:13 +0545

    table api refactor

--- app/api/customerOrder/[id]/route.js
+++ app/api/customerOrder/[id]/route.js
@@ -1,10 +1,11 @@
 import { NextResponse } from "next/server";
 import { connectMongoDB } from "../../../../lib/mongodb";
 import CustomerOrder from "../../../../models/customerOrder";
+import { dbConnect } from "../../dbConnect";
 
 export async function GET(request, {params}) {
     const { id } = params;
-    await connectMongoDB();
+    await dbConnect(); // Reused MongoDB connection
     const customerorders = await CustomerOrder.findOne({ _id: id });
     return NextResponse.json({ customerorders }, { status: 200 });
 }
--- app/api/customerOrder/route.js
+++ app/api/customerOrder/route.js
@@ -1,10 +1,11 @@
 import { NextResponse } from "next/server";
 import { connectMongoDB } from "../../../lib/mongodb";
 import CustomerOrder from "../../../models/customerOrder";
+import { dbConnect } from "../dbConnect";
 
 export async function DELETE(request) {
     const id = request.nextUrl.searchParams.get("id");
-    await connectMongoDB();
+    await dbConnect(); // Reused MongoDB connection
     await CustomerOrder.findByIdAndDelete(id);
     return NextResponse.json({ message: "Order deleted." });
 }

78748e2646bd7f605d1c90ddc420f61ebdbaae2d
tekkon <tekkon@tekkons-MacBook-Pro.local>
2024-11-13 16:06:45 +0545

    remove sign up

--- app/api/register/route.js
+++ app/api/register/route.js
@@ -1,20 +1,20 @@
-import { NextResponse } from "next/server";
-import { connectMongoDB } from "../../../lib/mongodb";
-import User from "../../../models/user";
-import bcrypt from "bcryptjs";
+// import { NextResponse } from "next/server";
+// import { connectMongoDB } from "../../../lib/mongodb";
+// import User from "../../../models/user";
+// import bcrypt from "bcryptjs";
 
-export async function POST(req) {
-    try {
-      const { name, email, password } = await req.json();
-      const hashedPassword = await bcrypt.hash(password, 10);
-      await connectMongoDB();
-      await User.create({ name, email, password: hashedPassword });
+// export async function POST(req) {
+//     try {
+//       const { name, email, password } = await req.json();
+//       const hashedPassword = await bcrypt.hash(password, 10);
+//       await connectMongoDB();
+//       await User.create({ name, email, password: hashedPassword });
   
-      return NextResponse.json({ message: "User registered." }, { status: 201 });
-    } catch (error) {
-      return NextResponse.json(
-        { message: "An error occurred while registering the user." },
-        { status: 500 }
-      );
-    }
-  }
+//       return NextResponse.json({ message: "User registered." }, { status: 201 });
+//     } catch (error) {
+//       return NextResponse.json(
+//         { message: "An error occurred while registering the user." },
+//         { status: 500 }
+//       );
+//     }
+//   }

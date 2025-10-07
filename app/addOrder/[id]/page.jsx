03e40177518cbfb73ebf794b6d74f4bf6851f406
tekkon <tekkon@tekkons-MacBook-Pro.local>
2025-10-08 01:42:13 +0545

    add order ui changes

--- app/addOrder/[id]/page.jsx
+++ app/addOrder/[id]/page.jsx
@@ -590,7 +590,7 @@ export default function AddOrder({ params }) {
                   order_type: item.order_type // Individual order type
               }));
   
-              const res = await fetch("http://localhost:3000/api/orders", {
+              const res = await fetch("/api/orders", {
                   method: "POST",
                   headers: {
                       "Content-type": "application/json",
